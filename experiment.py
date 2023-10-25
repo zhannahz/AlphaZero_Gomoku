update the move file function, correct game steps calculation, and add in color in important prints

experiment.py
@@ -25,9 +25,9 @@ path = r"C:\Users\汉那\gitHubTracking\AlphaZero_Gomoku"
 
 
 def main():
-    print("\033[31mThis is red text!")
-    print("\033[32mThis is green text!")
-    print("\033[0mThis is reset to default color!")
+    #print("\033[31mThis is red text!")
+    #print("\033[32mThis is green text!")
+    #print("\033[0mThis is reset to default color!")
 
     global params
     params = {
@@ -53,9 +53,16 @@ def main():
         params["model"] = int(inputs[2])
 
     print("Participant ID: ", params["participant_id"])
-    print("Condition (0: block, 1: interchange): ", params["condition"])
-    print("Rule to start with (0: four-in-a-row, 1: knobby): ", params["model"])
 
+    if params["condition"] == 0:
+        print("Condition is learning by block")
+    else:
+        print("Condition is learning by interchange")
+
+    if params["model"] == 0:
+        print("Rule to start with is four-in-a-row")
+    else:
+        print("Rule to start with is knobby")
     store_params_to_file()
 
     update_with_condition(True)
@@ -138,7 +145,7 @@ def start_a_game():
 def end_experiment():
     params = load_params_from_file()
     move_files_with_id(params["participant_id"])
-    print("Experiment is complete. Thank you for participating!")
+    print("\033[31mExperiment is complete. Thank you for participating!")
 
 def count_trials():
     global params
@@ -158,7 +165,10 @@ def count_trials():
 
 
 def call_human_play():
-    print("Current game rule (0: 4iar, 1: knobby): ", params["model"])
+    if params["model"] == 0:
+        print("\033[31mCurrent game rule is four in a row")
+    else:
+        print("\033[31mCurrent game rule is knobby")
     subprocess.call(['python', 'human_play.py'])
 
 
@@ -171,7 +181,6 @@ def store_params_to_file(filename="params.json"):
     with open(filename, 'w') as file:
         json.dump(params, file)
 
-
 def move_files_with_id(participant_id):
     # Specify the pattern for files starting with the participant_id
     pattern = f"{participant_id}_*"
@@ -181,20 +190,19 @@ def move_files_with_id(participant_id):
         os.makedirs(destination_dir)
 
     # Move each file matching the pattern to the destination directory
-    #for file in glob.glob(pattern):
-    files_with_id = get_files_with_prefix(participant_id)
-    for file in files_with_id:
-        shutil.move(file, destination_dir)
+    file_path = os.path.join(os.getcwd(), "Data")
+    files = os.listdir(file_path)
+    for filename in files:
+        #print(filename)
+        if filename.startswith(f"{participant_id}_"):
+            #print(filename, "is moved")
+            f = os.path.join(file_path, filename)
+            shutil.move(f, destination_dir)
+
     # Also move the params.json file
     if os.path.exists("params.json"):
         shutil.move("params.json", destination_dir)
 
-def get_files_with_prefix(participant_id):
-    current_directory = os.getcwd()
-    all_files = os.listdir(current_directory)
-    pattern = f"{participant_id}_*"
-    matched_files = fnmatch.filter(all_files, pattern)
-    return matched_files
 def board_to_matrix(self):
     """Converts current board state to a matrix representation."""
     matrix = np.zeros((self.width, self.height), dtype=int)